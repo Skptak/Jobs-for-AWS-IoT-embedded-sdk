f1cdfadc8e141a394fe5faf7e53e878ee85cba6d
Soren Ptak <ptaksoren@gmail.com>
2023-08-03 07:08:40 -0700

    Swapping to clang-format

--- jobsFilePaths.cmake
+++ jobsFilePaths.cmake
@@ -1,12 +1,9 @@
-# This file adds source files and include directories
-# into variables for use from different repositories
-# in their Cmake based build systems.  Only the library
-# files are added.
+# This file adds source files and include directories into variables for use
+# from different repositories in their Cmake based build systems.  Only the
+# library files are added.
 
 # JOBS library source files.
-set( JOBS_SOURCES
-     ${CMAKE_CURRENT_LIST_DIR}/source/jobs.c )
+set(JOBS_SOURCES ${CMAKE_CURRENT_LIST_DIR}/source/jobs.c)
 
 # JOBS library Public Include directories.
-set( JOBS_INCLUDE_PUBLIC_DIRS
-     ${CMAKE_CURRENT_LIST_DIR}/source/include )
+set(JOBS_INCLUDE_PUBLIC_DIRS ${CMAKE_CURRENT_LIST_DIR}/source/include)
